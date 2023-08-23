4a1b49edb1ecca5f2d5ba23308af6b762679b2f2
xdyssze <53408336+xdyssze@users.noreply.github.com>
2023-08-23 12:02:13 +0200

    Implemented the beggining of symbols and placement

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
     /*
     TODO:
@@ -6,12 +7,37 @@ public class Main {
       * fixa algorytm för att avgöra vinnare
      */
     public static void main(String[] args) {
-        Square SQUARE = new Square();
+        Scanner scan = new Scanner(System.in);
+        boolean running = true;
+        char PLAYERCHAR, COMPUTERCHAR;
+
+        Square sqr = new Square();
         // jag vill göra ett tictactoe spel som på det smartaste vägen möjligt kollar vem som vunnit
         // allt annat e bara onödigt,
 
+        System.out.println("Du ska nu få spela TIC TAC TOE, Välj vilken Symbol du vill vara:");
+        String tempstr = scan.nextLine();
+        // Ser till att man inte kan stoppa in nått gay såsom ingeting / mellanslag och att det inte är samma tecken som datorn
+        if(tempstr.isEmpty() || tempstr.charAt(0) == ' ') {
+            PLAYERCHAR = 'X';
+            COMPUTERCHAR = 'O';
+
+        } else {
+            PLAYERCHAR = tempstr.charAt(0);
+            if(PLAYERCHAR == 'O') {
+                COMPUTERCHAR = 'X';
+            }
+        }
+        while(running) {
+            System.out.println(sqr.gridString());
+
+
+
+
+
+        }
+
     }
-    // denna ska rita rutan
 
     public static class Square {
         // finns bara 3 olika täcken som kan vara i en square, "o", "x" eller " "
@@ -21,15 +47,20 @@ public class Main {
         }
         void initSquare(){
             for(char[] y_axis : grid) {
-                for(char x_axis : y_axis) {
-                    x_axis = ' ';
+                for(int i = 0; i < 3; i++) {
+                    y_axis[i] = ' ';
                 }
             }
         }
         public String gridString() {
             String grdStr = "";
+        // top part
+            for(int i = 0; i < 3; i++) {
+                grdStr += "--1----------\r\n";
+                grdStr += "| " + grid[i][0] + " | "+ grid[i][1] + " | "+ grid[i][2] + " |\r\n";
 
-
+            }
+            grdStr += "-------------";
             return(grdStr);
         }
         // kollar vinnaren av en viss ruta
